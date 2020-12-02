9dfb2b8fcd653c5fcce8835918d4ef0c06393eea
Nicolas Huberty <huberty.nicolas@hotmail.com>
2020-12-02 20:36:04 +0100

    Working version with the FilmItem beginning git add filmItem.js Modify me plz

--- Components/Search.js
+++ Components/Search.js
@@ -1,13 +1,36 @@
 import React from 'react'
-import {View,TextInput,Button} from 'react-native'
+import {FlatList,StyleSheet,Text, View,TextInput,Button} from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './filmItem'
+
 class Search extends React.Component{
     render(){
         return(
-            <View style={{marginTop: 40}}>
-                <TextInput placeholder='Nom de la ville'/>
+            <View style={styles.main_container}>
+                <TextInput style={styles.textInput}placeholder='Nom de la ville'/>
                 <Button title='Rechercher' onPress={()=>{}}/>
+                <FlatList
+                    data={films}
+                    keyExtractor={(item)=>item.id.toString()}
+                    renderItem={({item}) => <FilmItem film={item}/>}
+                />
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    TextInput: {
+        marginLeft:5,
+        marginRight:5,
+        height:50,
+        borderColor:'#000000',
+        borderWidth:1,
+        paddingLeft:5
+    },
+    main_container:{
+        flex:1,
+        marginTop:20
+     
+    }
+})
 export default Search
